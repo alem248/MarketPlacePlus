256055fa40b40d80ef24f35c6a242332fea61387
alem248 <https://github.com/alem248>
2026-06-12 09:34:39 -0500

    Finalizando cambios en la rama de limpieza

--- routes/web.php
+++ routes/web.php
@@ -39,18 +39,18 @@ Route::middleware(['auth'])->group(function () {
     // Gestión de productos del vendedor
     Route::get('/seller/products/create', [ProductController::class, 'create'])->name('seller.products.create');
     Route::post('/seller/products', [ProductController::class, 'store'])->name('seller.products.store');
-<<<<<<< HEAD
+
     Route::get('/seller/products/{id}/edit', [ProductController::class, 'edit'])->name('seller.products.edit');
     Route::put('/seller/products/{id}', [ProductController::class, 'update'])->name('seller.products.update');
     Route::post('/seller/products/{id}/acknowledge', [App\Http\Controllers\ProductController::class, 'acknowledge'])
     ->name('seller.products.acknowledge');
-=======
+
 
     Route::get('/tratos', [TratosController::class, 'index'])->name('tratos.index');
 
     // Detalle/seguimiento de un trato específico del comprador
     Route::get('/tratos/{trato}', [TratosController::class, 'show'])->name('tratos.show');
->>>>>>> 0c905f4df2eb846b652ae3ba3f27768d528c12c7
+
 });
 
 // Ruta de vista simple (Independiente)
